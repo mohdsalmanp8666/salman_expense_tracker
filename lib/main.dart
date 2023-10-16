33142e53013f99bb98dc943ef79bd71be47058ec
Mohd.Salman <mohdsalmanp866@gmail.com>
2023-10-16 18:13:26 +0530

    Firebase Initialized

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:provider/provider.dart';
 import 'package:salman_expense_tracker/providers/home/home_provider.dart';
@@ -9,6 +10,7 @@ import 'package:salman_expense_tracker/views/transaction/transaction_screen.dart
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
